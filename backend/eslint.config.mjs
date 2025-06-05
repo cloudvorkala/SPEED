6c7a45d259e9f6d5fc73e62dcb6882d804084852
cloudvorkala <cloudvorkala@gmail.com>
2025-06-05 15:12:01 +1200

    updated ESLint setting

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -26,7 +26,7 @@ export default [
       '@typescript-eslint': tseslint,
     },
     rules: {
-      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs['recommended'].rules,
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn'
